623f8aaef949055bc99dcb8e095dac4ee021a0fd
Prabir Kumar Majee <136071633+prabirkr@users.noreply.github.com>
2024-01-27 19:07:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+
+function App() {
+  return (
+    <div >
+      <Profile/>
+    </div>
+  );
+}
+
+export default App;
+
+// 1.  npm install @material-ui/core //  npm install @mui/material @mui/styled-engine-sc styled-components
+// 2.  npm install @material-ui/icons  //  npm install @mui/icons-material
